95d8215913e0465807913b990e25ac717126f8bb
Miro Nurmela <miro.nurmela@gmail.com>
2014-01-12 13:29:43 +0200

    Expandable list base implementation

--- HospitalHelper/src/main/java/com/minu/hospitalhelper/ItemDetailFragment.java
+++ HospitalHelper/src/main/java/com/minu/hospitalhelper/ItemDetailFragment.java
@@ -2,7 +2,7 @@ package com.minu.hospitalhelper;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import android.widget.ExpandableListView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +10,9 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.minu.hospitalhelper.content.Content;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A fragment representing a single Item detail screen.
@@ -78,7 +81,14 @@ public class ItemDetailFragment extends Fragment {
             } else if (mItem.id == 4) {
                 // Medical events
                 rootView = inflater.inflate(R.layout.events, container, false);
-                ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
+                ExpandableListView elv = (ExpandableListView) rootView.findViewById(R.id.expandableList);
+                ArrayList<String> headers = generateHeaders();
+                HashMap<String, ArrayList<String>> items = generateItems();
+                ExpandableListAdapter ela = new ExpandableListAdapter(this.getActivity(), headers, items);
+                elv.setAdapter(ela);
+                System.out.println(headers);
+                System.out.println(items);
+               // ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.content);
             } else if (mItem.id == 5) {
                 // Medical history
                 rootView = inflater.inflate(R.layout.history, container, false);
@@ -96,5 +106,21 @@ public class ItemDetailFragment extends Fragment {
         }*/
 
         return rootView;
+    }
+
+    private ArrayList<String> generateHeaders() {
+        ArrayList<String> results = new ArrayList<String>();
+        results.add("Visit on the doctor at 23.9.2013");
+        results.add("Visit on the doctor at 10.6.2013");
+        return results;
+    }
+
+    private HashMap<String, ArrayList<String>> generateItems() {
+        HashMap<String, ArrayList<String>> results = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> things = new ArrayList<String>();
+        things.add("Things!");
+        results.put("Visit on the doctor at 23.9.2013", things);
+        results.put("Visit on the doctor at 10.6.2013", things);
+        return results;
     }
 }
